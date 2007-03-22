5865d5a0560add5dcc33cd09492e8e64fcd2bbb3
Andrey Tarantsov <andreyvit@gmail.com>
2007-03-22 14:09:36 +0600

    [svn] Created initial IDE UI project.

---
+++ com.yoursway.ide.ui/src/com/yoursway/ide/ui/Activator.java
@@ -0,0 +1,49 @@
+package com.yoursway.ide.ui;
+
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+    
+    public static final String PLUGIN_ID = "com.yoursway.ide.ui";
+    
+    private static Activator plugin;
+    
+    public Activator() {
+    }
+    
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+    
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+    
+    public static Activator getDefault() {
+        return plugin;
+    }
+    
+    /**
+     * Returns an image descriptor for the image file at the given plug-in
+     * relative path
+     * 
+     * @param path
+     *            the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
+    
+    public static void log(Throwable e) {
+        getDefault().getLog().log(new Status(Status.ERROR, PLUGIN_ID, e.getMessage(), e));
+    }
+}
---
+++ com.yoursway.ide.ui/src/com/yoursway/ide/ui/perspectives/RailsPerspective.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.yoursway.ide.ui.perspectives;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
+
+/**
+ * Defines the layout of the primary perspective for Rails development.
+ * 
+ * @author Andrey Tarantsov
+ */
+public class RailsPerspective implements IPerspectiveFactory {
+    
+    private IPageLayout factory;
+    
+    public RailsPerspective() {
+        super();
+    }
+    
+    public void createInitialLayout(IPageLayout factory) {
+        this.factory = factory;
+        addViews();
+        addActionSets();
+        addNewWizardShortcuts();
+        addPerspectiveShortcuts();
+        addViewShortcuts();
+    }
+    
+    private void addViews() {
+        IPlaceholderFolderLayout bottom = factory.createPlaceholderFolder("bottom", IPageLayout.BOTTOM,
+                0.75f, factory.getEditorArea());
+        bottom.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+        bottom.addPlaceholder(ThirdPartyInterfaceComponentsConstants.GENERIC_HISTORY_VIEW);
+        bottom.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+        bottom.addPlaceholder(IPageLayout.ID_TASK_LIST);
+        bottom.addPlaceholder(ThirdPartyInterfaceComponentsConstants.RUBY_DOCUMENTATION_VIEW);
+        bottom.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+        bottom.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+        bottom.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
+        
+        IFolderLayout right = factory.createFolder("right", IPageLayout.RIGHT, 0.7f, factory.getEditorArea());
+        right.addView(ThirdPartyInterfaceComponentsConstants.DLTK_SCRIPT_EXPLORER_VIEW);
+        right.addPlaceholder(IPageLayout.ID_OUTLINE);
+        
+        // IFolderLayout rightBottom = factory.createFolder("rightBottom",
+        // IPageLayout.BOTTOM, 0.3f, "right");
+        // factory.addFastView(CVS_REPOSITORIES_VIEW, 0.50f);
+        // factory.addFastView(TEAM_SYNCHRONIZE_VIEW, 0.50f);
+    }
+    
+    private void addActionSets() {
+        // factory.addActionSet(ThirdPartyInterfaceComponentsConstants.LAUNCH_ACTION_SET);
+        // factory.addActionSet(ThirdPartyInterfaceComponentsConstants.DEBUG_ACTION_SET);
+        // factory.addActionSet(ThirdPartyInterfaceComponentsConstants.PROFILE_ACTION_SET);
+        // factory.addActionSet(ThirdPartyInterfaceComponentsConstants.TEAM_UI_ACTION_SET);
+        // factory.addActionSet(ThirdPartyInterfaceComponentsConstants.CVS_UI_ACTION_SET);
+        // factory.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+    }
+    
+    private void addPerspectiveShortcuts() {
+        factory.addPerspectiveShortcut(ThirdPartyInterfaceComponentsConstants.TEAM_SYNCHRONIZING_PERSPECTIVE);
+        factory.addPerspectiveShortcut(ThirdPartyInterfaceComponentsConstants.CVS_PERSPECTIVE);
+        factory.addPerspectiveShortcut(ThirdPartyInterfaceComponentsConstants.RESOURCE_PERSPECTIVE);
+    }
+    
+    private void addNewWizardShortcuts() {
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.NEW_PROJECT_CHECKOUT_WIZARD);
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.RUBY_NEW_PROJECT_WIZARD);
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.RUBY_NEW_FILE_WIZARD);
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.NEW_FOLDER_WIZARD);
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.NEW_FILE_WIZARD);
+        factory.addNewWizardShortcut(ThirdPartyInterfaceComponentsConstants.NEW_UNTITLED_TEXT_FILE_WIZARD);
+    }
+    
+    private void addViewShortcuts() {
+        factory.addShowViewShortcut(ThirdPartyInterfaceComponentsConstants.ANT_VIEW);
+        factory.addShowViewShortcut(ThirdPartyInterfaceComponentsConstants.CVS_ANNOTATE_VIEW);
+        factory.addShowViewShortcut(ThirdPartyInterfaceComponentsConstants.GENERIC_HISTORY_VIEW);
+        factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+        factory.addShowViewShortcut(IPageLayout.ID_RES_NAV);
+        factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+        factory.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+        factory.addShowViewShortcut(IProgressConstants.PROGRESS_VIEW_ID);
+    }
+    
+}
