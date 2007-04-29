5d5e0a99bed0480bb989da3fbc73b6c36be8ebdd
Mikhail Gusarov <dottedmag@dottedmag.net>
2007-04-29 22:25:34 +0700

    [svn] Build fix: save current $CWD before cd.

--- com.yoursway.ide-build/build.sh
+++ com.yoursway.ide-build/build.sh
@@ -4,13 +4,15 @@ JAVA=java
 ECLIPSE_DIR=/home/builder/eclipse
 BASEBUILD_DIR=/home/builder/org.eclipse.releng.basebuilder
 
+CURDIR=$PWD
+
 (cd $BASEBUILD_DIR/plugins/org.eclipse.pde.build/scripts; \
  $JAVA -jar $ECLIPSE_DIR/plugins/org.eclipse.platform_*/startup.jar \
         -application org.eclipse.ant.core.antRunner \
         -console \
         -consoleLog \
         -install $ECLIPSE_DIR \
-        -Dbuilder=$PWD \
-        -DbuildDirectory=$PWD/work \
+        -Dbuilder=$CURDIR \
+        -DbuildDirectory=$CURDIR/work \
         -DbaseLocation=$ECLIPSE_DIR \
 )
