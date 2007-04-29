c5a0d8c4364111291917b1a372859fc770a422a2
Mikhail Gusarov <dottedmag@dottedmag.net>
2007-04-29 22:24:08 +0700

    [svn] Fixed build script: cd to PDEbuild scripts dir.

--- com.yoursway.ide-build/build.sh
+++ com.yoursway.ide-build/build.sh
@@ -4,11 +4,13 @@ JAVA=java
 ECLIPSE_DIR=/home/builder/eclipse
 BASEBUILD_DIR=/home/builder/org.eclipse.releng.basebuilder
 
-$JAVA -jar $ECLIPSE_DIR/plugins/org.eclipse.platform_*/startup.jar \
-       -application org.eclipse.ant.core.antRunner \
-       -console \
-       -consoleLog \
-       -install $ECLIPSE_DIR \
-       -Dbuilder=$PWD \
-       -DbuildDirectory=$PWD/work \
-       -DbaseLocation=$ECLIPSE_DIR
+(cd $BASEBUILD_DIR/plugins/org.eclipse.pde.build/scripts; \
+ $JAVA -jar $ECLIPSE_DIR/plugins/org.eclipse.platform_*/startup.jar \
+        -application org.eclipse.ant.core.antRunner \
+        -console \
+        -consoleLog \
+        -install $ECLIPSE_DIR \
+        -Dbuilder=$PWD \
+        -DbuildDirectory=$PWD/work \
+        -DbaseLocation=$ECLIPSE_DIR \
+)
