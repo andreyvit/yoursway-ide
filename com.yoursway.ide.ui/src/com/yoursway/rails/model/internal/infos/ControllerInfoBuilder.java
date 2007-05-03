c0c721228dd7549847587fa89f806cdd9bb0acf1
Mikhail Gusarov <dottedmag@dottedmag.net>
2007-05-04 03:04:50 +0700

    [svn] Reverted previous commit as missing files are added to repo.

--- com.yoursway.ide.ui/src/com/yoursway/rails/model/internal/infos/ControllerInfoBuilder.java
+++ com.yoursway.ide.ui/src/com/yoursway/rails/model/internal/infos/ControllerInfoBuilder.java
@@ -5,36 +5,40 @@ import java.util.Collection;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
 
 import com.yoursway.utils.RailsFileUtils;
 import com.yoursway.utils.RailsNamingConventions;
+import com.yoursway.utils.ResourceSwitch;
 
 public class ControllerInfoBuilder {
     
-    //    private final class CollectingVisitor extends ResourceSwitch {
-    //        @Override
-    //        protected boolean visitFile(IFile resource) {
-    //            if (canAddControllerFor(resource))
-    //                addController(resource);
-    //            return false;
-    //        }
-    //        
-    //        @Override
-    //        protected boolean visitFolder(IFolder resource) {
-    //            return true;
-    //        }
-    //        
-    //        @Override
-    //        protected boolean visitProject(IProject resource) {
-    //            throw new AssertionError("Cannot happen");
-    //        }
-    //        
-    //        @Override
-    //        protected boolean visitRoot(IWorkspaceRoot resource) {
-    //            throw new AssertionError("Cannot happen");
-    //        }
-    //    }
+    private final class CollectingVisitor extends ResourceSwitch {
+        @Override
+        protected boolean visitFile(IFile resource) {
+            if (canAddControllerFor(resource))
+                addController(resource);
+            return false;
+        }
+        
+        @Override
+        protected boolean visitFolder(IFolder resource) {
+            return true;
+        }
+        
+        @Override
+        protected boolean visitProject(IProject resource) {
+            throw new AssertionError("Cannot happen");
+        }
+        
+        @Override
+        protected boolean visitRoot(IWorkspaceRoot resource) {
+            throw new AssertionError("Cannot happen");
+        }
+    }
     
     private final ArrayList<ControllerInfo> result = new ArrayList<ControllerInfo>();
     private final ProjectInfo projectInfo;
@@ -47,12 +51,13 @@ public class ControllerInfoBuilder {
     public Collection<ControllerInfo> build() {
         IFolder controllersFolder = projectInfo.getProject().getFolder(
                 RailsNamingConventions.APP_CONTROLLERS_PATH);
-        //        if (controllersFolder.exists())
-        //            try {
-        //                controllersFolder.accept(new CollectingVisitor());
-        //            } catch (CoreException e) {
-        //                Activator.unexpectedError(e);
-        //            }
+        if (controllersFolder.exists())
+            try {
+                controllersFolder.accept(new CollectingVisitor());
+            } catch (CoreException e) {
+                //FIXME: Activator no longer has unexpectedError()
+                //Activator.unexpectedError(e);
+            }
         return result;
     }
     
