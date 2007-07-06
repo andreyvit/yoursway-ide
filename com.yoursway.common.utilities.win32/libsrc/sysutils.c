8cc4e81e83af6a3a04ef8dc8860540f6c7076b7f
Andrey Tarantsov <andreyvit@gmail.com>
2007-07-07 03:35:22 +0700

    [svn] Oops, forgot to use correct filename for testing. Also replaces 0 and 1 with JNI_FALSE and JNI_TRUE :)

--- com.yoursway.common.utilities.win32/libsrc/sysutils.c
+++ com.yoursway.common.utilities.win32/libsrc/sysutils.c
@@ -48,38 +48,46 @@ JNIEXPORT jstring JNICALL Java_com_yoursway_utils_SystemUtilitiesImpl_getApplica
 JNIEXPORT jboolean JNICALL Java_com_yoursway_utils_SystemUtilitiesImpl_isConsoleApplication
   (JNIEnv *env, jobject object, jstring filenamejs)
 {
-	LPCTSTR filename = L"TEST";
+	int length;
+	const jchar *filenamenz;
 	HANDLE hFile;
 	HANDLE hFileMapping;
 	LPVOID lpFileBase;
-	jboolean result = 0;
 	PIMAGE_DOS_HEADER dosHeader;
+	TCHAR filename[MAX_PATH+1];
+	jboolean result = JNI_FALSE;
+	
+	length = (*env)->GetStringLength( env, filenamejs );
+	filenamenz = (*env)->GetStringChars(env, filenamejs, NULL);
+	memcpy(filename, filenamenz, min(MAX_PATH * sizeof(TCHAR), length * sizeof(TCHAR)));
+	filename[length] = 0;
+	(*env)->ReleaseStringChars(env, filenamejs, filenamenz);
+	
 	hFile = CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, NULL,
 		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 	if ( hFile == INVALID_HANDLE_VALUE ) {
 		printf("Couldn't open file with CreateFile()\n");
-		return 0;
+		return JNI_FALSE;
 	}
 	hFileMapping = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
 	if ( hFileMapping == 0 ) {
 		CloseHandle(hFile);
 		printf("Couldn't open file mapping with CreateFileMapping()\n");
-		return 0;
+		return JNI_FALSE;
 	}
 	lpFileBase = MapViewOfFile(hFileMapping, FILE_MAP_READ, 0, 0, 0);
 	if ( lpFileBase == 0 ) {
 		CloseHandle(hFileMapping);
 		CloseHandle(hFile);
 		printf("Couldn't map view of file with MapViewOfFile()\n");
-		return 0;
+		return JNI_FALSE;
 	}
-	printf("Dump of file %s\n\n", filename);
 	dosHeader = (PIMAGE_DOS_HEADER)lpFileBase;
 	if ( dosHeader->e_magic == IMAGE_DOS_SIGNATURE ) {
 		IMAGE_NT_HEADERS *pNTHeader = (IMAGE_NT_HEADERS *) ((char *) dosHeader + dosHeader->e_lfanew);
 		WORD subsys = pNTHeader->OptionalHeader.Subsystem;
 		if (subsys == IMAGE_SUBSYSTEM_WINDOWS_CUI)
-			result = 1;
+			result = JNI_TRUE;
 	} else {
 	}
 	UnmapViewOfFile(lpFileBase);
