18e04f46864b175b94c5c3b03e9a7db0c1398ed5
Andrey Tarantsov <andreyvit@gmail.com>
2007-07-29 21:13:28 +0700

    [svn] Obscure wierd changes in tests, dunno what it is, really

--- com.yoursway.databinding.resources.tests/src/com/yoursway/databinding/resources/tests/shallow/AbstractShallowChildrenTests.java
+++ com.yoursway.databinding.resources.tests/src/com/yoursway/databinding/resources/tests/shallow/AbstractShallowChildrenTests.java
@@ -1,15 +1,10 @@
 package com.yoursway.databinding.resources.tests.shallow;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import org.eclipse.core.databinding.observable.set.IObservableSet;
-import org.eclipse.core.databinding.observable.set.ISetChangeListener;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.junit.After;
 import org.junit.Before;
 
 import com.yoursway.databinding.resources.ResourceObservables;
