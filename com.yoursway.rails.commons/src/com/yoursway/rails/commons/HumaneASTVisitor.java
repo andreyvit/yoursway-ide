cf829d587faf015c1b7bef1acdebe8b4a7ca91b9
Andrey Tarantsov <andreyvit@gmail.com>
2007-08-19 16:45:10 +0700

    Humane Ruby AST visitor moved to a commons plugin. Added support for visiting ModuleDeclaration.

--- com.yoursway.rails.commons/src/com/yoursway/rails/commons/HumaneASTVisitor.java
+++ com.yoursway.rails.commons/src/com/yoursway/rails/commons/HumaneASTVisitor.java
@@ -1,8 +1,9 @@
-package com.yoursway.utils;
+package com.yoursway.rails.commons;
 
 import org.eclipse.dltk.ast.ASTNode;
 import org.eclipse.dltk.ast.ASTVisitor;
 import org.eclipse.dltk.ast.declarations.MethodDeclaration;
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.ast.expressions.CallExpression;
 import org.eclipse.dltk.ruby.ast.RubySingletonMethodDeclaration;
 
@@ -76,6 +77,15 @@ public class HumaneASTVisitor {
         leaveNode(node);
     }
     
+    protected boolean enterModuleDeclaration(ModuleDeclaration node) {
+        return enterNode(node);
+    }
+    
+    protected void leaveModuleDeclaration(ModuleDeclaration node) {
+        leaveNode(node);
+    }
+    
+    // copy this and the next method to support a new node
     protected boolean enter(MethodDeclaration node) {
         return enterNode(node);
     }
@@ -105,14 +115,17 @@ public class HumaneASTVisitor {
             return enterSingletonMethodDeclaration((RubySingletonMethodDeclaration) node);
         else if (node instanceof MethodDeclaration)
             return enterMethodDeclaration((MethodDeclaration) node);
-        else if (node instanceof MethodDeclaration)
-            return enterMethodDeclaration((MethodDeclaration) node);
         else if (node instanceof CallExpression)
             return enterCall((CallExpression) node);
+        else if (node instanceof ModuleDeclaration)
+            return enterModuleDeclaration((ModuleDeclaration) node);
+        // copy the following two lines to support a new node
+        else if (node instanceof MethodDeclaration)
+            return enter((MethodDeclaration) node);
         else
             return enterUnknown(node);
     }
-    
+
     public final void switchLeave(ASTNode node) {
         nestedLevelDropped(--nestingLevel);
         if (node instanceof RubySingletonMethodDeclaration)
@@ -121,6 +134,11 @@ public class HumaneASTVisitor {
             leaveMethodDeclaration((MethodDeclaration) node);
         else if (node instanceof CallExpression)
             leaveCall((CallExpression) node);
+        else if (node instanceof ModuleDeclaration)
+            leaveModuleDeclaration((ModuleDeclaration) node);
+        // copy the following two lines to support a new node
+        else if (node instanceof MethodDeclaration)
+            leave((MethodDeclaration) node);
         else
             leaveUnknown(node);
     }
@@ -132,8 +150,10 @@ public class HumaneASTVisitor {
     public void traverse(ASTNode node) {
         try {
             node.traverse(adapter);
+        } catch (RuntimeException e) {
+            throw e;
         } catch (Exception e) {
-            throw new RuntimeException(ExceptionUtils.unwrap(e));
+            throw new RuntimeException(e);
         }
     }
 }
