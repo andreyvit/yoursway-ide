a8393606aa6ea2f227fe06a4e6065184093cf663
Andrey Tarantsov <andreyvit@gmail.com>
2007-12-29 00:27:34 +0600

    Reformatted two more files.

--- com.yoursway.rails.model/src/com/yoursway/model/rails/IRailsModelRoot.java
+++ com.yoursway.rails.model/src/com/yoursway/model/rails/IRailsModelRoot.java
@@ -7,9 +7,9 @@ import com.yoursway.model.repository.IModelRoot;
 import com.yoursway.model.resource.IResourceProject;
 
 public interface IRailsModelRoot extends IModelRoot {
-    
-    IHandle<Collection<IRailsApplicationProject>> projects();
-    
-    IHandle<IRailsApplicationProject> mapProject(IResourceProject resourceProject);
-    
+
+	IHandle<Collection<IRailsApplicationProject>> projects();
+
+	IRailsApplicationProject mapProject(IResourceProject resourceProject);
+
 }
--- com.yoursway.rails.model/src/com/yoursway/model/rails/impl/RogerRabbitResolver.java
+++ com.yoursway.rails.model/src/com/yoursway/model/rails/impl/RogerRabbitResolver.java
@@ -9,46 +9,58 @@ import com.yoursway.model.repository.IModelRootProvider;
 import com.yoursway.model.repository.IResolver;
 
 public class RogerRabbitResolver implements IResolver {
-    
-    private IModelRootProvider modelRootProvider;
-    private DependencyRequestor dependencyRequestor;
-    
-    public RogerRabbitResolver(IModelRootProvider modelRootProvider, DependencyRequestor dependencyRequestor) {
-        this.modelRootProvider = modelRootProvider;
-        this.dependencyRequestor = dependencyRequestor;
-    }
-    
-    public <V> Collection<? extends V> changedHandles(Class<V> handleInterface) {
-        return null;
-    }
-    
-    public void checkCancellation() {
-    }
-    
-    public <V, H extends IHandle<V>> V get(H handle) {
-        dependencyRequestor.dependency(handle);
-        if (handle instanceof RabbitHandle) {
-            RabbitHandle<V> h = (RabbitHandle<V>) handle;
-            return h.getData();
-        }
-        if (handle instanceof RabbitFamilyHandle) {
-            RabbitFamilyHandle<V> familyHandle = (RabbitFamilyHandle<V>) handle;
-            return (V) familyHandle.getData();
-        }
-        return null;
-    }
-    
-    public <V, H extends IHandle<V>> V getIfAvail(H handle) {
-        dependencyRequestor.dependency(handle);
-        if (handle instanceof RabbitHandle) {
-            RabbitHandle<V> h = (RabbitHandle<V>) handle;
-            return h.getData();
-        }
-        return null;
-    }
-    
-    public <V extends IModelRoot> V obtainRoot(Class<V> rootHandleInterface) {
-        return modelRootProvider.obtainRoot(rootHandleInterface);
-    }
-    
+
+	private final IModelRootProvider modelRootProvider;
+	private final DependencyRequestor dependencyRequestor;
+
+	public RogerRabbitResolver(IModelRootProvider modelRootProvider,
+			DependencyRequestor dependencyRequestor) {
+		this.modelRootProvider = modelRootProvider;
+		this.dependencyRequestor = dependencyRequestor;
+	}
+
+	public <V> Collection<? extends V> changedHandles(Class<V> handleInterface) {
+		return null;
+	}
+
+	public void checkCancellation() {
+	}
+
+	public <V, H extends IHandle<V>> V get(H handle) {
+		dependencyRequestor.dependency(handle);
+		if (handle instanceof RabbitHandle) {
+			RabbitHandle<V> h = (RabbitHandle<V>) handle;
+			return h.getData();
+		}
+		if (handle instanceof RabbitFamilyHandle) {
+			RabbitFamilyHandle<V> familyHandle = (RabbitFamilyHandle<V>) handle;
+			return (V) familyHandle.getData();
+		}
+		return null;
+	}
+
+	public <V, H extends IHandle<V>> V getIfAvail(H handle) {
+		dependencyRequestor.dependency(handle);
+		if (handle instanceof RabbitHandle) {
+			RabbitHandle<V> h = (RabbitHandle<V>) handle;
+			return h.getData();
+		}
+		return null;
+	}
+
+	public <V extends IModelRoot> V obtainRoot(Class<V> rootHandleInterface) {
+		return modelRootProvider.obtainRoot(rootHandleInterface);
+	}
+
+	public Collection<? extends IHandle<?>> changedHandlesForModel(
+			Class<?> rootHandleInterface) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void dontKillForLaterAccess() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
