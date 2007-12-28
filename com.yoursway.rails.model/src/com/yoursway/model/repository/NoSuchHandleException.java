d7e36b3adafebf857d3fc430fec247230c8359aa
Andrey Tarantsov <andreyvit@gmail.com>
2007-12-29 00:14:07 +0600

    Turned NoSuchHandleException into a non-checked exception.

--- com.yoursway.rails.model/src/com/yoursway/model/repository/NoSuchHandleException.java
+++ com.yoursway.rails.model/src/com/yoursway/model/repository/NoSuchHandleException.java
@@ -1,10 +1,10 @@
 package com.yoursway.model.repository;
 
-public class NoSuchHandleException extends Exception {
-    
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 5728866186309420594L;
-    
+public class NoSuchHandleException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5728866186309420594L;
+
 }
