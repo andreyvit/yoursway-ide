d4e87e08425a26e82b00730cd8a5a4f7973cd1cd
Andrey Tarantsov <andreyvit@gmail.com>
2007-12-29 23:47:40 +0600

    Described multithreading contract details for IResolver.

--- com.yoursway.genericmodel.core/src/com/yoursway/model/repository/IResolver.java
+++ com.yoursway.genericmodel.core/src/com/yoursway/model/repository/IResolver.java
@@ -2,6 +2,9 @@ package com.yoursway.model.repository;
 
 import java.util.Collection;
 
+/**
+ * The implementations must be safe for multitheaded usage.
+ */
 public interface IResolver extends IModelRootProvider {
     
     <V, H extends IHandle<V>> V get(H handle) throws NoSuchHandleException;
