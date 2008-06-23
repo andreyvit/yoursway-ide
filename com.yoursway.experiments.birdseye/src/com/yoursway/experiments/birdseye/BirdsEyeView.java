c822e5b0d9b49e8055f5d59e5dd8ba71468c03ef
Andrey Tarantsov <andreyvit@gmail.com>
2008-06-23 13:44:28 +0700

    Added flipping to the Bird's Eye View (flips to a dummy settings page).

--- com.yoursway.experiments.birdseye/src/com/yoursway/experiments/birdseye/BirdsEyeView.java
+++ com.yoursway.experiments.birdseye/src/com/yoursway/experiments/birdseye/BirdsEyeView.java
@@ -7,6 +7,7 @@ import static com.yoursway.utils.YsCollections.addIfNotNull;
 import static com.yoursway.utils.YsPathUtils.extension;
 import static com.yoursway.utils.YsStrings.emptyToNull;
 import static java.util.Collections.emptyList;
+import static org.eclipse.jface.layout.GridDataFactory.swtDefaults;
 import static org.eclipse.jface.layout.GridLayoutFactory.fillDefaults;
 
 import java.io.File;
@@ -15,22 +16,40 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.ViewPart;
 
 import com.yoursway.experiments.birdseye.model.Container;
 import com.yoursway.experiments.birdseye.model.Leaf;
 import com.yoursway.experiments.birdseye.model.Node;
+import com.yoursway.swt.animations.flip.Flipper;
+import com.yoursway.swt.animations.flip.FlipperListener;
+import com.yoursway.swt.animations.flip.StackLayoutFlipperListener;
 
 public class BirdsEyeView extends ViewPart implements BirdsEyeListener {
     
-    private BirdsEyeComposite birdsEyeComposite;
+    private BirdsEyeComposite birdsEyeDisplay;
     private Label selectionDescription;
+    private Composite stack;
+    private Composite settingsComposite;
+    private Flipper flipper;
+    private Composite birdsEyeContainer;
     
     private static class Factory {
         
@@ -76,10 +95,25 @@ public class BirdsEyeView extends ViewPart implements BirdsEyeListener {
     @Override
     public void createPartControl(Composite parent) {
         parent.setLayout(fillDefaults().margins(5, 5).spacing(0, 0).create());
-        birdsEyeComposite = new BirdsEyeComposite(parent, SWT.NONE);
-        birdsEyeComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         
-        selectionDescription = new Label(parent, SWT.NONE);
+        stack = new Composite(parent, SWT.NO_BACKGROUND);
+        stack.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        stack.setLayout(new StackLayout());
+        createBirdsEyeComposite(stack);
+        createSettingsComposite(stack);
+        
+        flipper = new Flipper(birdsEyeContainer, settingsComposite);
+        new StackLayoutFlipperListener(flipper, stack);
+    }
+
+    private void createBirdsEyeComposite(Composite parent) {
+        birdsEyeContainer = new Composite(parent, SWT.NONE); 
+        birdsEyeContainer.setLayout(fillDefaults().margins(0, 0).spacing(0, 0).create());
+        
+        birdsEyeDisplay = new BirdsEyeComposite(birdsEyeContainer, SWT.NONE);
+        birdsEyeDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        
+        selectionDescription = new Label(birdsEyeContainer, SWT.NONE);
         selectionDescription.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
         
         Factory factory = new Factory(parent.getDisplay());
@@ -103,12 +137,61 @@ public class BirdsEyeView extends ViewPart implements BirdsEyeListener {
         File rootFile = new File("/Users/andreyvit/Projects/ujudge/ujudge");
         Node root = collect(rootFile, factory, "");
         
-        birdsEyeComposite.display(root);
-        birdsEyeComposite.addListener(this);
+        birdsEyeDisplay.display(root);
+        birdsEyeDisplay.addListener(this);
         
         applySmallSize(parent);
+        
+        birdsEyeDisplay.addListener(SWT.MouseDown, new Listener() {
+
+            public void handleEvent(Event event) {
+                flipper.flip();
+            }
+            
+        });
     }
     
+    private void createSettingsComposite(Composite parent) {
+        settingsComposite = new Composite(parent, SWT.NONE);
+        settingsComposite.setLayout(new GridLayout(1, false));
+        addBorder(settingsComposite);
+        
+        applySmallSize(settingsComposite);
+        
+        Label label = new Label(settingsComposite, SWT.NONE);
+        label.setText("Working sets to show:");
+        
+        new Button(settingsComposite, SWT.CHECK).setText("YourSway IDE");
+        new Button(settingsComposite, SWT.CHECK).setText("SADR");
+        new Button(settingsComposite, SWT.CHECK).setText("EskoArtwork");
+        
+        Button settingsButton = new Button(settingsComposite, SWT.PUSH);
+        settingsButton.setText("Done");
+        settingsButton.setLayoutData(swtDefaults().indent(0, 8).create());
+        settingsButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                flipper.flip();
+            }
+        });
+        
+        for (Control control : settingsComposite.getChildren())
+            control.setFont(settingsComposite.getFont());
+    }
+
+    private void addBorder(final Composite composite) {
+        composite.addPaintListener(new PaintListener() {
+            
+            public void paintControl(PaintEvent e) {
+                GC gc = e.gc;
+                Rectangle clientArea = composite.getClientArea();
+                gc.setForeground(e.display.getSystemColor(SWT.COLOR_BLACK));
+                gc.drawRectangle(clientArea.x, clientArea.y, clientArea.width - 1, clientArea.height - 1);
+                //                gc.drawRectangle(clientArea.x+1, clientArea.y+1, clientArea.width - 3, clientArea.height - 3);
+            }
+            
+        });
+    }
+
     private Node collect(File entry, Factory factory, String prefix) {
         if (entry.isFile())
             return factory.createLeaf(entry, prefix + entry.getName());
@@ -133,7 +216,7 @@ public class BirdsEyeView extends ViewPart implements BirdsEyeListener {
     
     @Override
     public void setFocus() {
-        birdsEyeComposite.setFocus();
+        birdsEyeDisplay.setFocus();
     }
 
     public void birdsEyeHovered(Leaf node, Event event) {
