15c4af523532a251adc307b919d63feda794da19
Andrey Tarantsov <andreyvit@gmail.com>
2008-06-29 08:27:05 +0700

    Ported com.yoursway.ide.application to not depend on org.eclipse.ui.

--- com.yoursway.ide.application/src/com/yoursway/ide/application/controllers/mainwindow/IDocumentContentController.java
+++
@@ -1,5 +0,0 @@
-package com.yoursway.ide.application.controllers.mainwindow;
-
-public interface IDocumentContentController {
-    
-}
--- com.yoursway.ide.application/src/com/yoursway/ide/application/internal/Activator.java
+++ com.yoursway.ide.application/src/com/yoursway/ide/application/internal/Activator.java
@@ -1,22 +1,20 @@
 package com.yoursway.ide.application.internal;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
 	public static final String PLUGIN_ID = "com.yoursway.ide.application";
 
 	private static Activator plugin;
 	
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-		super.stop(context);
 	}
 
 	public static Activator getDefault() {
