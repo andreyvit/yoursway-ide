844b31263c7bc96f3ae0e40ba037d84150fdbf21
Alexander Sheboltaev <lliypik@oldmacbook.local>
2008-07-21 18:35:48 +0700

    Worksheet: only one thread for animations.

--- com.yoursway.ide.interactiveconsole/src/com/yoursway/ide/worksheet/view/Animation.java
+++ com.yoursway.ide.interactiveconsole/src/com/yoursway/ide/worksheet/view/Animation.java
@@ -1,9 +1,12 @@
 package com.yoursway.ide.worksheet.view;
 
+import java.lang.Thread.State;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Animation {
     
     private AnimationUpdater updater;
-    private Thread thread;
     
     private volatile int targetWidth = 0;
     private volatile int targetHeight = 0;
@@ -12,77 +15,89 @@ public class Animation {
     private volatile boolean instantWidth = false;
     private volatile int width = 0;
     private volatile int height = 0;
+    private volatile int alpha = 0;
+    
+    private int xDelay = 0;
+    private int yDelay = 0;
+    
+    private static final int d = 2;
+    private static final int delay = 10;
+    
+    private static final Collection<Animation> animations = new ConcurrentLinkedQueue<Animation>();
+    private static final Thread thread = new Thread(Animation.class.getSimpleName()) {
+        
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    for (Animation animation : animations) {
+                        if (animation.isDisposed())
+                            continue;
+                        
+                        animation.updateSize();
+                        animation.updateAlpha();
+                    }
+                    sleep(10);
+                }
+            } catch (InterruptedException e) {
+                interrupted();
+            }
+        }
+        
+    };
     
     public void start(final AnimationUpdater updater) {
         this.updater = updater;
         
-        thread = new Thread(Animation.class.getSimpleName()) {
-            private int alpha = 0;
-            private final int d = 2;
-            
-            private int xDelay = 0;
-            private int yDelay = 0;
-            private final int delay = 10;
-            
-            @Override
-            public void run() {
-                try {
-                    while (true) {
-                        updateSize();
-                        updateAlpha();
-                        sleep(10);
-                    }
-                } catch (InterruptedException e) {
-                    interrupted();
-                }
-            }
-            
-            private void updateSize() {
-                int x = f(targetWidth - width, d);
-                if (x < 0) {
-                    if (xDelay < delay) {
-                        xDelay++;
-                        x = 0;
-                    }
-                } else {
-                    xDelay = 0;
-                }
-                
-                int y = f(targetHeight - height, d);
-                if (y < 0) {
-                    if (yDelay < delay) {
-                        yDelay++;
-                        y = 0;
-                    }
-                } else {
-                    yDelay = 0;
-                }
-                
-                if (x != 0 || y != 0) {
-                    synchronized (Animation.this) {
-                        if (instantWidth)
-                            instantWidth = false;
-                        else
-                            width += x;
-                    }
-                    
-                    height += y;
-                    
-                    updater.updateSize(width, height);
-                }
-            }
-            
-            private void updateAlpha() {
-                int a = f(targetAlpha - alpha, d * 3);
-                if (a != 0) {
-                    alpha += a;
-                    updater.updateAlpha(alpha);
-                }
-            }
-        };
+        animations.add(this);
         
-        thread.start();
+        if (thread.getState() == State.NEW) {
+            thread.setDaemon(true);
+            thread.start();
+        }
+    }
+    
+    private void updateSize() {
+        int x = f(targetWidth - width, d);
+        if (x < 0) {
+            if (xDelay < delay) {
+                xDelay++;
+                x = 0;
+            }
+        } else {
+            xDelay = 0;
+        }
         
+        int y = f(targetHeight - height, d);
+        if (y < 0) {
+            if (yDelay < delay) {
+                yDelay++;
+                y = 0;
+            }
+        } else {
+            yDelay = 0;
+        }
+        
+        if (x != 0 || y != 0) {
+            synchronized (Animation.this) {
+                if (instantWidth)
+                    instantWidth = false;
+                else
+                    width += x;
+            }
+            
+            height += y;
+            
+            updater.updateSize(width, height);
+        }
+    }
+    
+    private void updateAlpha() {
+        int a = f(targetAlpha - alpha, d * 3);
+        if (a != 0) {
+            alpha += a;
+            updater.updateAlpha(alpha);
+        }
     }
     
     public void targetSize(int width, int height) {
@@ -114,11 +129,12 @@ public class Animation {
     public void dispose() {
         if (isDisposed())
             return;
-        thread.interrupt();
+        
+        animations.remove(this);
     }
     
     public boolean isDisposed() {
-        return thread.isInterrupted();
+        return !animations.contains(this);
     }
     
 }
