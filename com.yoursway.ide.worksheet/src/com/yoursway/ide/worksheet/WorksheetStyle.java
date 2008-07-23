807c801d18561dad10cfa9ec9455af8ff8585ed4
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2008-07-23 18:03:25 +0700

    Worksheet: ResultBlock.init refactored.

--- com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/WorksheetStyle.java
+++ com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/WorksheetStyle.java
@@ -1,11 +1,18 @@
 package com.yoursway.ide.worksheet;
 
 import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 
 public interface WorksheetStyle {
     
     Font worksheetFont();
+    
+    Font resultFont();
+    
+    Color resultBlockColor();
+    
+    Color outputColor();
     
     StyleRange errorStyle(int offset, int length);
     
--- com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/internal/demo/StyleMock.java
+++ com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/internal/demo/StyleMock.java
@@ -1,5 +1,6 @@
 package com.yoursway.ide.worksheet.internal.demo;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
@@ -10,15 +11,36 @@ import com.yoursway.ide.worksheet.WorksheetStyle;
 public class StyleMock implements WorksheetStyle {
     
     private final Font font;
+    private final Font resultFont;
+    
+    private final Color resultBackColor;
+    private final Color outputColor;
     private final Color errorColor;
     
     public StyleMock(Display display) {
         font = new Font(display, "Monaco", 12, 0);
+        resultFont = new Font(display, "Monaco", 12, SWT.BOLD);
+        
+        resultBackColor = new Color(display, 100, 100, 100);
+        outputColor = new Color(display, 255, 255, 255);
         errorColor = new Color(display, 255, 192, 192);
+        
     }
     
     public Font worksheetFont() {
         return font;
+    }
+    
+    public Font resultFont() {
+        return resultFont;
+    }
+    
+    public Color resultBlockColor() {
+        return resultBackColor;
+    }
+    
+    public Color outputColor() {
+        return outputColor;
     }
     
     public StyleRange errorStyle(int start, int length) {
