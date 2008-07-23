f7e26aa63af42cf2a5f1f44928f738aee9e988e5
Andrey Tarantsov <andreyvit@gmail.com>
2008-07-23 13:32:15 +0700

    Extracted ExtendedStyledText into com.yoursway.swt.styledtext in common repository.

--- com.yoursway.ide.interactiveconsole/src/com/yoursway/swt/styledtext/insertions/internal/YourSwayStyledTextEventSource.java
+++
@@ -1,9 +0,0 @@
-package com.yoursway.swt.styledtext.insertions.internal;
-
-import com.yoursway.swt.styledtext.insertions.ResizeListener;
-
-public interface YourSwayStyledTextEventSource {
-    
-    void addResizeListener(ResizeListener listener);
-    
-}
