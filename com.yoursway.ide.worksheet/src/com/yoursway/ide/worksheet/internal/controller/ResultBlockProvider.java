d445a6ac790e2054be95e3f3258ca0bb84994026
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2008-07-23 16:28:58 +0700

    Worksheet: code quality - annotations & null checks.

--- com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/internal/controller/ResultBlockProvider.java
+++ com.yoursway.ide.worksheet/src/com/yoursway/ide/worksheet/internal/controller/ResultBlockProvider.java
@@ -1,9 +1,11 @@
 package com.yoursway.ide.worksheet.internal.controller;
 
 import com.yoursway.ide.worksheet.internal.view.ResultBlock;
+import com.yoursway.utils.annotations.UseFromUIThread;
 
 public interface ResultBlockProvider {
     
+    @UseFromUIThread
     ResultBlock get();
     
 }
