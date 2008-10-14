f499165968f70f9ec3aa706c6ce03210b7ec61a6
Yuri Baburov <burchik@gmail.com>
2008-10-15 05:12:47 +0700

    Added completion and implemented saving.

--- com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowImpl.java
+++ com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowImpl.java
@@ -1,21 +1,31 @@
 package com.yoursway.ide.editors.text;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import net.sf.colorer.ParserFactory;
 import net.sf.colorer.eclipse.ColorerPlugin;
 import net.sf.colorer.swt.ColorManager;
 import net.sf.colorer.swt.TextColorer;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.internal.databinding.provisional.swt.ControlUpdater;
 import org.eclipse.jface.text.Document;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.MessageBox;
 
+import com.yoursway.completion.demo.DictionaryCompletion;
+import com.yoursway.completion.gui.CompletionController;
 import com.yoursway.ide.application.view.mainwindow.EditorWindow;
 
 public class DocumentContentWindowImpl implements DocumentContentWindow {
@@ -65,8 +75,32 @@ public class DocumentContentWindowImpl implements DocumentContentWindow {
         textColorer.setCross(true, true);
         textColorer.setRegionMapper("default", true);
         
-//        text.addVer
+		try {
+			new CompletionController(text,new DictionaryCompletion());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         
+        
+        text.addModifyListener(new ModifyListener(){
+        	public void modifyText(ModifyEvent e) {
+				try {
+					FileWriter fileWriter = new FileWriter(model.file().getValue());
+	        		try {
+						fileWriter.write(text.getText());
+					} catch (IOException e1) {
+						MessageDialog.openError(null, "Great news!", "Oops #28...");
+						e1.printStackTrace();
+					} finally {
+						fileWriter.close();
+					}
+				} catch (IOException e2) {
+					MessageDialog.openError(null, "Great news!", "Oops #23...");
+					e2.printStackTrace();
+				}
+        	}
+        });
+
         new ControlUpdater(text) {
             protected void updateControl() {
                 File file = model.file().getValue();
