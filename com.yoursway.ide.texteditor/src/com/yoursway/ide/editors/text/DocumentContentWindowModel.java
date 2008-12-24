b674d8c3f3ee6e5a52ad40b6ca72f1ea10d5d065
Andrey Tarantsov <andreyvit@gmail.com>
2008-12-24 11:18:15 +0600

    Fixed compilation error in com.yoursway.ide.texteditor.

--- com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowModel.java
+++ com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowModel.java
@@ -1,9 +1,13 @@
 package com.yoursway.ide.editors.text;
 
+import java.io.File;
+
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 
 public interface DocumentContentWindowModel {
     
     IObservableValue<String> data();
+
+	IObservableValue<File> file();
     
 }
--- com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowModelImpl.java
+++ com.yoursway.ide.texteditor/src/com/yoursway/ide/editors/text/DocumentContentWindowModelImpl.java
@@ -1,14 +1,22 @@
 package com.yoursway.ide.editors.text;
 
+import java.io.File;
+
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 
 public class DocumentContentWindowModelImpl implements DocumentContentWindowModel {
     
     public final IObservableValue<String> data = WritableValue.withValueType(String.class);
+    
+    public final IObservableValue<File> file = WritableValue.withValueType(String.class);
 
     public IObservableValue<String> data() {
         return data;
     }
+
+	public IObservableValue<File> file() {
+		return file;
+	}
     
 }
