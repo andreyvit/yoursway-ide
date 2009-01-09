8a771beed7dc6d087fe1eeac2bafe8ba4a95fc69
Yuri Baburov <burchik@gmail.com>
2009-01-09 19:01:06 +0600

    Worked around catching browser text updates problem. Warning: 3.5M4 or greater is now prerequisite

---
+++ com.yoursway.web.editing/src/com/yoursway/web/editing/DocumentChangeListener.java
@@ -0,0 +1,5 @@
+package com.yoursway.web.editing;
+
+public abstract class DocumentChangeListener {
+	abstract void changed(String newContents);
+}
---
+++ com.yoursway.web.editing/src/com/yoursway/web/editing/editor.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('keyup', function(e) {
+  jDocumentChanged(document.body.innerHTML);
+});
+
+document.body.contentEditable='true';document.designMode='on';
